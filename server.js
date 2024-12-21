d9c9dabdb5b86fd3d945817f68e88d76936c0c70
Alan Biju <alanbiju1234@gmail.com>
2024-12-21 17:12:39 +0000

    initliased basic structure of single page web app

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve the dataset
+app.get('/dataset.json', (req, res) => {
+    const dataset = fs.readFileSync('./dataset.json', 'utf8');
+    res.json(JSON.parse(dataset));
+});
+
+// Start server
+const PORT = process.env.PORT || 5005;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
